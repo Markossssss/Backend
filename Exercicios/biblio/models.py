ea9e99561ca9429ea9c894300831072487928cc8
Markossssss <marcospachecoiaw.2018@gmail.com>
2019-09-24 21:13:23 -0300

    Adicionadas funcionálidades à biblioteca

--- Exercicios/biblio/models.py
+++ Exercicios/biblio/models.py
@@ -8,4 +8,40 @@ class Biblioteca(models.Model):
     ISBN = models.IntegerField()
     ano_publicacao = models.DateField()
 
+class Autor(models.Model):
+    class Meta:
+        verbose_name_plural = "autores"
+    nome = models.CharField(max_length=50)
+    sobrenome = models.CharField(max_length=50)
+
+    def __str__(self):
+        return self.sobrenome.upper() + ','+ self.nome
+
+class Aluno(models.Model):
+    matricula = models.CharField('Matrícula', max_length=12, unique= True)
+    nome = models.CharField('Nome',max_length=50)
+    data_nascimento = models.DateField('Data de Nascimento')
+    email = models.EmailField('E-mail', max_length=100)
+
+    def __str__ (self):
+        return self.nome
+
+class Livro (models.Model):
+    titulo = models.CharField('Título',max_length=100)
+    autor = models.ForeignKey(Autor, on_delete=models.CASCADE)
+    ano_publicacao = models.IntegerField('Ano de Publicação')
+
+    def __str__(self):
+        return "{}, ({})".format(self.titulo, self.ano_publicacao)
+
+class Emprestimo(models.Model):
+    usuario = models.ForeignKey('auth.user', on_delete=models.CASCADE)
+    aluno = models.ForeignKey(Aluno, on_delete=models.CASCADE)
+    data_retirada = models.DateField()
+    data_devolucao = models.DateField()
+    livros = models.ManyToManyField(Livro)
+    devolvido = models.BooleanField()
+
+
+
 # Create your models here.
